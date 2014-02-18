1c3cb9c17f14ff5dca7b72c2598598f76c1b4d3e
Alexandru Manea <alexandru.m.manea@gmail.com>
2014-02-18 11:20:20 +0200

    - Initial commit

---
+++ [Study] Algorithms/src/unionfind/Solution_QuickFind.java
@@ -0,0 +1,105 @@
+package unionfind;
+
+public class Solution_QuickFind {
+
+	/**
+	 * 
+	 * ****************************************************************************************************************
+	 * QUICK FIND :: EAGER APPROACH TO SOLVING THE UNION-FIND PROBLEM WHICH USES AN INTEGER ARRAY INDEXED BY THE OBJECT 
+	 * AS THE SUPPORTING DATA STRUCTURE. 
+	 * 
+	 * THE INTERPRETATION IS THAT TWO OBJECTS ARE CONNECTED IFF THEIR ENTRIES IN THE ARRAY ARE THE SAME.
+	 * ****************************************************************************************************************
+	 * 
+	 *  e.g.
+	 *  
+	 *  0   1---2   3---4
+	 *  |       |   |   |
+	 *  |       |   |   |
+	 *  5---6   7   8   9
+	 *  
+	 *       0 1 2 3 4 5 6 7 8 9
+	 * id[]  | | | | | | | | | |   => {0, 5, 6}, {1, 2, 7}, {3, 4, 8, 9}
+	 *       0 1 1 8 8 0 0 1 8 8
+	 *   
+	 *       
+	 * # FIND  :: Check if p and q have the same id
+	 * 
+	 * e.g. id[1] != id[6] => not connected
+	 *      id[3] == id[4] => connected
+	 * 
+	 * 
+	 * # UNION :: Change all entries whose id equals id[p] to value id[q] 
+	 * 
+	 * e.g. UNION(1, 6)
+	 * 
+	 *          0 1 2 3 4 5 6 7 8 9
+	 * => id[]  | | | | | | | | | |   => {0, 1, 2, 5, 6, 7}, {3, 4, 8, 9}
+	 *          0 0 0 8 8 0 0 0 8 8    
+	 *          
+	 *           
+	 * Implementation of above algorithm bellow.
+	 *       
+	 */
+	
+	
+	/*
+	 * Quick Find implementation. 
+	 * 
+	 */
+	
+	static class QuickFindUF {
+		
+		// data structure used
+		private int[] ids;
+		
+		public QuickFindUF(int n) {
+			
+			this.ids = new int[n];
+			
+			for (int i = 0; i < n; i++) {
+				
+				ids[i] = i;
+			}
+		}
+		
+		// # FIND
+		public boolean connected(int p, int q) {
+			
+			return (ids[p] == ids[q]);
+		}
+		
+		// # UNION
+		public void union(int p, int q) {
+			
+			int pId = ids[p];
+			int qId = ids[q];
+			
+			for (int i = 0; i < ids.length; i++) {
+				
+				if (ids[i] == pId) {
+					
+					ids[i] = qId;
+				}
+			}
+		}
+	}
+	
+	
+	/**
+	 * COMPLEXITY ANALYSIS OF QUICK-FIND
+	 * ---------------------------------
+	 * 
+	 * Cost model -- number of array read/write
+ 	 * 
+	 * # INITIALIZE :: n
+	 * 
+	 * # UNION      :: O(n)
+	 * # FIND       :: O(1)
+	 * 
+	 * *******************************************************************************************************
+	 * IN THE QUICK-FIND ALGORITHM, THE FIND OPERATION IS FAST (O(1)), BUT UNION OPERATION IS EXPENSIVE (O(n))
+	 * *******************************************************************************************************
+	 * 
+	 */
+}
---
+++ [Study] Algorithms/src/unionfind/Solution_QuickUnion.java
@@ -0,0 +1,19 @@
+package unionfind;
+
+public class Solution_QuickUnion {
+
+	
+	/**
+	 * 
+	 * ****************************************************************************************************************
+	 * QUICK UNION :: LAZY APPROACH TO SOLVING THE UNION-FIND PROBLEM WHICH USES THE SAME INTEGER ARRAY INDEX BY THE
+	 * OBJECT.
+	 * 
+	 * THE INTERPRETATION OF THE ARRAY IN THIS CASE IS THAT IT REPRESENTS A FOREST (MULTIPLE TREES)
+	 * ****************************************************************************************************************
+	 *
+	 */
+	
+	
+	
+}
